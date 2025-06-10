9346dfc38833f15f2a78138f9ce6c04c6c4ef0ea
gabei <gabe1792@gmail.com>
2025-06-10 15:30:37 -0500

    Setup basic input handleres

--- ltcl-client/app/page.js
+++ ltcl-client/app/page.js
@@ -1,11 +1,25 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isbn, setIsbn] = useState("");
+
+  const handleChange = (e) => {
+    setIsbn(e.target.value);
+  }
+
+  const handlesubmit = (e) => {
+    console.log(isbn);
+  }
+
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-      
+        <input value={isbn}  onChange={handleChange} placeholder="Enter ISBN"></input>
+        <button onClick={handlesubmit}>Search</button>
       </main>
     </div>
   );
