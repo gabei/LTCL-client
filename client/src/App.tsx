1b703ac94838915c52ab3b76461042fdf9ddf0e7
gabei <gabe1792@gmail.com>
2025-06-11 11:57:07 -0500

    Feature: Search now successfully renders a list of codes for copying.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,10 @@ import './App.css'
 
 function App() {
   const [isbn, setIsbn] = useState("");
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]);
   const [message, setMessage] = useState("");
 
-  const handleChange = (e:Event): => {
+  const handleChange = (e:Event) => {
     e.target.value ? setIsbn(e.target.value) : null
 
   }
@@ -18,18 +18,29 @@ function App() {
     setData(newData);
   }
 
-  const fetchData: = async(query:string): => {
-    const url = "https://localhost/search";
+  const fetchData = async(query:string) => {
+    const url = "http://localhost:8000/search?code=";
     const response = await fetch(url + query);
+    console.log(response);
     return response.json();
   }
+
+  const renderData = () => {
+    return (
+      <ul>
+        {data.map((item) => <li>{item}</li>)}
+      </ul>
+    )
+  }
+
+  const loadingMessage = <p>{message}</p>
 
 return (
     <div>
       <main>
-        <input value={isbn}  onChange={handleChange} placeholder="Enter ISBN"></input>
+        <input value={isbn} onChange={handleChange} placeholder="Enter ISBN"></input>
         <button onClick={handlesubmit}>Search</button>
-        <div>{data ? data : message}</div>
+        <div>{data ? renderData() : loadingMessage}</div>
       </main>
     </div>
   )
