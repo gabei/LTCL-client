f0b487eac50aabe79abbc3891b295deb208ee2ee
gabei <gabe1792@gmail.com>
2025-06-25 18:16:52 -0500

    install react error boundaries. not correctly implemented yet

--- client/src/BookData/BookData.tsx
+++ client/src/BookData/BookData.tsx
@@ -1,35 +1,44 @@
 import './BookData.css'
 import { type ApiResponse } from "../types/types"
+import { ErrorBoundary } from 'react-error-boundary';
 
 export default function BookData(data:ApiResponse) {
 
-  function handleCopy(data: string):void {
-      navigator.clipboard.writeText(data);
-  }
-      
-
-  function listItem(listHeading:string = "",data: string){
+  function listItem(heading:string = "",data: string){
     return (
       <li>
-        <strong>{listHeading}:</strong> {data}
+        <strong>{heading}:</strong> {data}
       </li>
     )
   }
 
-  return (
-    <div className="BookData">
-      <span className="warning">Double-check this information before making an ILL request!</span>
-      <ul className="BookData__list">
-        {listItem("Title", data.title)}
-        {listItem("Author", data.author)}
-        {listItem("Publisher", data.publisher)}
-        {listItem("ISBN", data.isbn)}
-        {listItem("OCLC", data.oclc)}
-      </ul>
-      <h2>Lender Codes</h2>
+  const errorFallback = <p>Something went wrong. Double check the supplied lookup code and try again.</p>
+
+  function LenderList(data:ApiResponse){
+    return (
       <ul className="lender-list">
         {data.lenderData.map((item:string) => <li>{item}</li>)}
       </ul>
-    </div>
+    )
+  }
+
+  
+  return (
+    
+      <div className="BookData">
+        <span className="warning">Double-check this information before making an ILL request!</span>
+        <ul className="BookData__list">
+          {listItem("Title", data.title)}
+          {listItem("Author", data.author)}
+          {listItem("Publisher", data.publisher)}
+          {listItem("ISBN", data.isbn)}
+          {listItem("OCLC", data.oclc)}
+        </ul>
+        <h2>Lender Codes</h2>
+        <ErrorBoundary fallback={errorFallback}>
+          <LenderList data={data}/>
+        </ErrorBoundary>
+      </div>
   )
+ 
 }
